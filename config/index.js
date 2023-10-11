c8e466495d31a44e0a356029e7eb31eef06b5367
HN-TreanT <hntreant23@gmail.com>
2023-10-11 23:05:10 +0700

    connect db

--- config/index.js
+++ config/index.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
 const config = {
   api: {
     port: 8080,
   },
   database: {
-    username: "admin",
-    password: "hnam23012002",
-    database: "qlttb",
-    port: 3306, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
-    host: "localhost",
+    // username: "root",
+    // password: "NAmgsxoLMu0FL52OQYgs",
+    // database: "railway",
+    // port: 6827, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
+    // host: "containers-us-west-202.railway.app",
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
     pool: {
       max: 5,
