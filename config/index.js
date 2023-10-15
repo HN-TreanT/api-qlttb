b92b0c230480768a4e494a864405dd4087904054
HN-TreanT <hntreant23@gmail.com>
2023-10-15 09:32:33 +0700

    update mysql

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ const config = {
     port: 8080,
   },
   database: {
-    // username: "root",
-    // password: "NAmgsxoLMu0FL52OQYgs",
-    // database: "railway",
-    // port: 6827, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
-    // host: "containers-us-west-202.railway.app",
+    // username: "uql6hlnmvcx7wa7f",
+    // password: "GziWUCgblp0hXOI2rOQp",
+    // database: "b72mn38a2xqx4gcobh41",
+    // port: 3306, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
+    // host: "b72mn38a2xqx4gcobh41-mysql.services.clever-cloud.com",
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
