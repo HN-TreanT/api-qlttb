b45ea44ab999515cd9b4a3ffaede0477eea4aa10
HN-TreanT <hntreant23@gmail.com>
2023-10-26 09:33:43 +0700

    fyyerew

--- config/index.js
+++ config/index.js
@@ -4,11 +4,17 @@ const config = {
     port: 8080,
   },
   database: {
+    // username: "uql6hlnmvcx7wa7f",
+    // password: "GziWUCgblp0hXOI2rOQp",
+    // database: "b72mn38a2xqx4gcobh41",
+    // port: 3306, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
+    // host: "b72mn38a2xqx4gcobh41-mysql.services.clever-cloud.com",
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQL_PORT, // thay đổi port kết nối tới MySQL từ 8080 thành 3307
     host: process.env.MYSQL_HOST,
+
     dialect: "mysql",
     pool: {
       max: 5,
