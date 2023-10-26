20b287b1ea3aaf190743089d806a6d48e32c0465
HN-TreanT <hntreant23@gmail.com>
2023-10-26 12:34:35 +0700

    fix reaque

--- routes/lich-bao-duong.route.js
+++ routes/lich-bao-duong.route.js
@@ -1,11 +1,11 @@
 const Route = require("express").Router();
 const controller = require("../controller/lich-bao-duong");
 const tryCatch = require("../middleware/tryCatch");
-
-Route.get("/",requireRole('A','U'),  tryCatch(controller.getAll));
-Route.get("/:id",requireRole('A','U'),  tryCatch(controller.getById));
-Route.post("/",requireRole('A'),  tryCatch(controller.create));
-Route.put("/:id",requireRole('A'),  controller.edit);
-Route.delete("/:id",requireRole('A'),  controller.deleteById);
+const {requireLogin, requireRole} = require("../middleware/auth")
+Route.get("/",requireLogin,requireRole('A','U'),  tryCatch(controller.getAll));
+Route.get("/:id",requireLogin,requireRole('A','U'),  tryCatch(controller.getById));
+Route.post("/",requireLogin,requireRole('A'),  tryCatch(controller.create));
+Route.put("/:id",requireLogin, requireRole('A'),  controller.edit);
+Route.delete("/:id",requireLogin, requireRole('A'),  controller.deleteById);
 
 module.exports = Route;
